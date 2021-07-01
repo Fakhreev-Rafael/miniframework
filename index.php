bbcb2424da85d2dcf1cfd2c6d878ae2ee03753e2
Fakhreev-Rafael <rafael-fakhreev@yandex.ru>
2021-07-01 18:28:28 +0300

    modified file index.php

--- index.php
+++ index.php
@@ -15,6 +15,24 @@
         }
     });
 
+    use app\core\Router;
+    use app\lib\request\RequestFactory;
+    use app\lib\route\Route;
+
+    use app\lib\exceptions\UserRoutesDidNotSetException;
+    use app\lib\exceptions\FileNotFoundException;
+    use app\lib\exceptions\ClassNotFoundException;
+    use app\lib\exceptions\MethodNotFoundException;
+
+    include_once('app/routes/routes.php');
     
+    try {
+        $router = new Router(RequestFactory::factory(), new Route());
+
+        $router->run();
+
+    } catch (UserRoutesDidNotSetException | FileNotFoundException | ClassNotFoundException | MethodNotFoundException $e) {
+        echo $e->customMessage();
+    }
 
 ?>
